4247b521ec152ffbfd8b601fbf9830aae44cfcb2
Ligre <ligreman@users.noreply.github.com>
2015-12-04 23:28:36 +0100

    Dialogo

---
+++ frontend/src/main/app/components/dialog/targetSelect/targetSelect.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular.module('kafhe.controllers')
+        .controller('Targetselect',
+            ['$scope', '$translate', 'skillId',
+                function ($scope, $translate, skillId) {
+                    $scope.skillId = skillId;
+                    /*********************************************************************/
+                    /*********************** FUNCIONES ***********************************/
+
+
+                }
+            ]);
+})();
--- frontend/src/main/app/directives/kfooterbattle/kfooterbattle.js
+++ frontend/src/main/app/directives/kfooterbattle/kfooterbattle.js
@@ -7,7 +7,49 @@
         return {
             restrict: 'E',
             replace: 'true',
-            templateUrl: 'app/directives/kfooterbattle/kfooterbattle.html'
+            scope: true,
+            templateUrl: 'app/directives/kfooterbattle/kfooterbattle.html',
+            controller: ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+
+                $scope.showTargets = fnShowTargets;
+
+                /**
+                 * Crea un cuadro de diálogo
+                 */
+                function fnShowTargets(skillId, $event) {
+                    $event.preventDefault();
+                    console.log(skillId);
+
+                    $mdDialog.show({
+                        locals: {
+                            skillId: skillId
+                        },
+                        controller: 'TargetSelect',
+                        templateUrl: 'app/components/dialogs/targetSelect/target-select.html',
+                        scope: $scope,
+                        preserveScope: true,
+                        clickOutsideToClose: false,
+                        escapeToClose: false
+                    }).then(function () {
+                        // Respuesta OK. Podría pasar un parámetro answer
+                    }, function () {
+                        // Ha respondido cancelar
+                    });
+                };
+
+                /**
+                 * Función que muestra u oculta el overlay
+                 * @param accepted Booleano que indica si se quiere mostrar u ocultar
+                 */
+                function fnInteractionDialog(accepted) {
+                    // Si he aceptado o cancelado el cuadro de diálogo
+                    if (accepted) {
+                        $mdDialog.hide();
+                    } else {
+                        $mdDialog.cancel();
+                    }
+                }
+            }]
         };
     });
 })();
